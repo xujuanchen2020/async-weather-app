12f987323d0bfcf2f2a4c0fb13a21adfa136e7e9
Xujuan.Chen <xujuan.chen@lc.cuny.edu>
2020-11-11 15:54:48 -0500

    wheather data fetched

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Form from "./Component/Form";
+import Titles from "./Component/Titles";
+import Weather from "./Component/Weather";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// const API_KEY = "37d2b27cd92b6113234c01fb234d23c1";
+
+class App extends React.Component { 
+  state = {
+    temperature: undefined,
+    city: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined,
+  }
+  getWeather = async(e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch("http://api.openweathermap.org/data/2.5/weather?q="+city+","+country+"&APPID=e6ffc5ebbe17c3aa660af150fd31572f")
+    const data = await api_call.json();
+    console.log(data)
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country:data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error:""
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Titles/>
+        <Form getWeather={this.getWeather}/>
+        <Weather/>
+      </div>
+    )
+  }
+
+} 
 
 export default App;
---
+++ src/Component/Weather.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Weather extends React.Component {
+    render(){
+        return (
+            <div>
+                Weather
+            </div>
+        )
+    }
+}
+
+export default Weather;
