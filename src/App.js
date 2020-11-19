594500c874129dc0b712e10efdbe97228a0094da
Xujuan.Chen <xujuan.chen@lc.cuny.edu>
2020-11-18 20:55:21 -0500

    display data

--- src/App.js
+++ src/App.js
@@ -3,31 +3,35 @@ import Form from "./Component/Form";
 import Titles from "./Component/Titles";
 import Weather from "./Component/Weather";
 
-// const API_KEY = "37d2b27cd92b6113234c01fb234d23c1";
-
 class App extends React.Component { 
   state = {
     temperature: undefined,
     city: undefined,
+    country: undefined,
     humidity: undefined,
     description: undefined,
     error: undefined,
   }
+
   getWeather = async(e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     const api_call = await fetch("http://api.openweathermap.org/data/2.5/weather?q="+city+","+country+"&APPID=e6ffc5ebbe17c3aa660af150fd31572f")
     const data = await api_call.json();
-    console.log(data)
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country:data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error:""
-    })
+
+    if(city&&country){
+      console.log(data)
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error:""
+      })
+    }
+
   }
 
   render() {
@@ -35,7 +39,14 @@ class App extends React.Component {
       <div>
         <Titles/>
         <Form getWeather={this.getWeather}/>
-        <Weather/>
+        <Weather 
+        temperature={this.state.temperature}
+        city={this.state.city}
+        country={this.state.country}
+        humidity={this.state.humidity}
+        description={this.state.description}
+        error={this.state.error}
+        />
       </div>
     )
   }
--- src/Component/Weather.js
+++ src/Component/Weather.js
@@ -4,7 +4,10 @@ class Weather extends React.Component {
     render(){
         return (
             <div>
-                Weather
+                {this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
+                {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+                {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+                {this.props.description && <p>Description: {this.props.description}, {this.props.error}</p>}
             </div>
         )
     }
