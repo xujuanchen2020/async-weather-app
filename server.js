68e842fb9ee0702ad49d4fa253e3648af14f75e2
Xujuan.Chen <xujuan.chen@lc.cuny.edu>
2020-11-30 09:24:18 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
-var port = normalizePort(process.env.PORT || 5000);
-var express = require('express');
-var app = express();
+const {createServer} = require('http');
+const express = require('express');
+const compression = require('compression');
+const morgan = require('morgan');
+const path = require('path');
+// const { normalize } = require('path');
+const normalizePort = port => parseInt(port,10);
+const PORT = normalizePort(process.env.PORT || 5000);
 
-app.use(express.static(__dirname));
-app.get('/', function(req, res) {
-  res.sender('index');
-})
+const app = express();
+const dev = app.get('env') !== 'production';
+// const port = 5000;
 
-server.listen(port, function(){
+if (!dev){
+  app.disable('x-powered-by');
+  app.use(compression());
+  app.use(morgan('common'));
+  app.use(express.static(path.resolve(__dirname,'build')));
+
+  app.get('*', (req, res) => {
+    res.send(path.resolve(__dirname,'build','index.html'));
+  })
+}
+
+if(dev){
+  app.use(morgan('dev'));
+}
+
+const server = createServer(app);
+
+server.listen(PORT, err=>{
+  if(err) throw err;
   console.log('Server started');
-})
+}) 
 
