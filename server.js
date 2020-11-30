4ec293c8782a09b70dbc06c2b5eb4795b0980870
Xujuan.Chen <xujuan.chen@lc.cuny.edu>
2020-11-29 22:52:43 -0500

    change server 5000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+var port = normalizePort(process.env.PORT || 5000);
 var express = require('express');
 var app = express();
-var port = normalizePort(process.env.PORT || 8080);
 
 app.use(express.static(__dirname));
 app.get('/', function(req, res) {
